1fc3a077fdca87260e5e454f3db66411bd84cbea
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-02-20 17:04:14 +0600

    ES6 intro

---
+++ js/let-const.js
@@ -0,0 +1,12 @@
+// no more use of var
+// let: let it to reassign
+// const: do not allow it to reassign
+
+let money = 25;
+money = 35;
+console.log(money);
+
+const bird = "jaan pakhi";
+// bird= 'moyna pakhi'
+const message = bird + " potas potas";
+console.log(message);
